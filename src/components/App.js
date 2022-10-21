a836c2aadc4ca6a05cc5400dcba5b2324066fabd
Shivang Bhardwaj <84633238+shivangbhardwaj2001@users.noreply.github.com>
2022-10-21 17:17:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect, Fragment } from "react";
 import '../styles/App.css';
+
 import List from "./List";
 
 const App = () => {
  
-//code here
+  const [value, setValue] = useState(0);
+  const [list, setList] = useState([]);
+
+
+  const onButtonClick = () => {
+    setList([])
+    for (let i = 1; i < Number(value)+1; i++) {
+      
+      setList((prev) => [...prev, i]);
+    }
+   
+  };
+ 
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
     <div id="main">
